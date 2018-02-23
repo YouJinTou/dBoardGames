54c28720a55c3f2985c22b770c52b187db4ee6f8
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-23 20:47:12 +0200

    Added a promisifier.

---
+++ public/javascripts/async.js
@@ -0,0 +1,10 @@
+const promisify = (inner) =>
+    new Promise((resolve, reject) =>
+        inner((err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        })
+    );

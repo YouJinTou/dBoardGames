8553311d0ebce409aedb9aeea7e2d4c93bd37c63
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-23 21:07:03 +0200

    All calls to web3 that fetch information now work asynchronously.

--- public/javascripts/Service.js
+++ public/javascripts/Service.js
@@ -1,16 +1,34 @@
 var Service = function () {
+    var self = this;
     var contracts = [
         '0xd9801934089743a17ba06180062b40ab15b596fb'
     ];
+
+    this.getInstance = function (address) {
+        return contract.prototype.at(address);
+    }
 
     this.getGames = function () {
         return contracts;
     }
 
+    this.getGame = async function (address) {
+        var instance = self.getInstance(address);
+        var game = {
+            currentMove: parseInt(await promisify(cb => instance.getHalfMovesCount(cb)) / 2),
+            playerToMove: await promisify(cb => instance.playerToMove(cb)),
+            durationPerMove: await promisify(cb => instance.getDurationPerMove(cb)),
+            fee: await promisify(cb => instance.getFee(cb)),
+            prizePool: await promisify(cb => instance.getPrizePool(cb)),
+        };
+
+        return game;
+    }
+
     this.getGameMoves = function (gameContract) {
-        var instance = contract.prototype.at(gameContract);
+        var instance = self.getInstance(gameContract);
         var moves = [];
-        
+
         instance.getHalfMovesCount.call((count) => {
             for (var i = 1; i < count; i++) {
                 instance.getHalfMove.call(i, (move) => {
@@ -22,7 +40,7 @@ var Service = function () {
         return moves;
     }
 
-    this.addGame= function (gameContract) {
+    this.addGame = function (gameContract) {
         contracts.push(gameContract);
     }
 };

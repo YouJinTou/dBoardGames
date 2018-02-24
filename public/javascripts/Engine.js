29622e2c1a2b25de9e7ac6c8bbfa36b612297cf2
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-24 15:05:51 +0200

    Added getEndGameCondition().

--- public/javascripts/Engine.js
+++ public/javascripts/Engine.js
@@ -1,6 +1,8 @@
 var Engine = function (address) {
     var self = this;
     var chess = new Chess();
+    this.board;
+
     var onDrop = function (source, target, piece, newPos, oldPos, orientation) {
         var sloppyNotation = source + '-' + target;
 
@@ -8,17 +10,39 @@ var Engine = function (address) {
             return 'snapback';
         }
 
-        service.makeMove(address, sloppyNotation);
+        service.makeMove(address, sloppyNotation, getEndGameCondition());
+    };
+
+    function getEndGameCondition() {
+        if (chess.in_checkmate()) {
+            return 'checkmate';
+        }
+
+        if (chess.in_draw()) {
+            return 'draw';
+        }
+
+        return 'checkmate';
+    }
+
+    var onDragStart = function (source, piece, position, orientation) {
+        console.log(chess.game_over())
+        if (chess.game_over() ||
+            (chess.turn() === 'w' && piece.search(/^b/) !== -1) ||
+            (chess.turn() === 'b' && piece.search(/^w/) !== -1)) {
+            return false;
+        }
     };
 
     this.config = {
         draggable: true,
         position: 'start',
+        onDragStart: onDragStart,
         onDrop: onDrop
     }
 
     this.setGame = function (moves) {
-        var board = ChessBoard('board', self.config);
+        board = ChessBoard('board', self.config);
 
         for (var m = 0; m < moves.length; m++) {
             makeMove(moves[m]);

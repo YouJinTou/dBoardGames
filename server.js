7839c083a11fb3f1200e26851933cf4d3d4ddf62
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-24 15:05:33 +0200

    Added /enforce.

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
+const validator = require('./validator.js');
+const web3 = require('./web3.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('index', {
     title: 'dBoardGames'
 }));
+
+app.post('/enforce', (req, res) => {
+    if (!req.body.contract || !req.body.condition) {
+        res.status(400).send();
+    }
+
+    if (!validator.conditionValid(req.body.contract, req.body.condition)) {
+        res.status(400).send();
+    }
+
+    res.status(200).send();
+});
 
 app.listen(3000, () => console.log('Running on port 3000.'));
 

088b4a6f8ae221726967464b88739dcf4fa3434e
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-24 16:48:37 +0200

    conditionValid turned into getGameCondition().

--- validator.js
+++ validator.js
@@ -1,24 +1,21 @@
 const web3 = require('./web3.js');
 var Chess = require('chess.js').Chess;
 
-exports.conditionValid = async function (contract, condition) {
+exports.getGameCondition = async function (contract) {
     var moves = await web3.getMoves(contract);
     var chess = new Chess();
-    var atLeastOneValid = false;
 
     for (var m = 0; m < moves.length; m++) {
-        if (!moves[m]) {
-            continue;
-        }
-
-        var move = chess.move(moves[m], { sloppy: true });
-
-        if (!move) {
-            return false;
-        }
-
-        atLeastOneValid = true;
+        chess.move(moves[m], { sloppy: true });
     }
-    
-    return atLeastOneValid;
+
+    if (chess.in_checkmate()) {
+        return 'checkmate';
+    }
+
+    if (chess.in_draw()) {
+        return 'draw';
+    }
+
+    return 'continue';
 };

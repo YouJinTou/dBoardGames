db00ebcd9b07b572b1dad77d3f8df070ef593024
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-25 12:13:17 +0200

    Added Port integration for Heroku.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -36,5 +37,5 @@ app.post('/enforce', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Running on port 3000.'));
+app.listen(port, () => console.log('Running on port 3000.'));
 
